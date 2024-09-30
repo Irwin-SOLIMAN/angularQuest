0f7aea909e21cb058f14224fb76fb589b0027f23
Irwin <irwin.soliman@gmail.com>
2024-09-30 16:22:33 +0200

    Quest Q6 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'article/:id',
         component: ArticlePageComponent,
+    },
+    {
+        path : 'contact',
+        component : ContactFormComponent,
     },
     {   path : '**',
         component : NotFoundComponent,
